8538dcda5c5cdc7b6994485bfd534f2eeed1f74e
mailtoShaniDevOps <shani.x424@gmail.com>
2024-12-14 13:23:08 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+# def goodbye(name,ending):
+#     print("shayan" + name)
+#     print(ending)
+    
+# goodbye(" harry","Thanks you")
+
+def goodbye(name,education,ending):
+    print("khan" + name )
+    print(education)
+    print(ending)
+    
+goodbye(" Harry","computer science","Thanks you ")
